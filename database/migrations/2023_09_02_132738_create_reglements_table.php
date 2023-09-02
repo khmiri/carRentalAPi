01f820581b68ffd245afbe3279c1469194f37883
khmiri <devsonastyl@gmail.com>
2023-09-02 15:11:29 +0100

    adding 5 tables fields

--- database/migrations/2023_09_02_132738_create_reglements_table.php
+++ database/migrations/2023_09_02_132738_create_reglements_table.php
@@ -13,8 +13,6 @@ return new class extends Migration
     {
         Schema::create('reglements', function (Blueprint $table) {
             $table->id();
-            $table->id();
-            $table->id();
             $table->timestamps();
         });
     }
